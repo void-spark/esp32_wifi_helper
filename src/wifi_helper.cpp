862216fdd5aa80c836bbe0d5176c563317a5bba3
void-spark <81029971+void-spark@users.noreply.github.com>
2021-06-05 14:06:07 +0200

    Update to latest API changes

--- src/wifi_helper.cpp
+++ src/wifi_helper.cpp
@@ -70,7 +70,7 @@ void wifiStart() {
 
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
